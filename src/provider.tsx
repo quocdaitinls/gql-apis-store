ff009f44fbbcb01d7a72556ce7c6fd65e5797625
QUOC DAI <quocdaitinls@gmail.com>
2022-03-14 01:09:13 +0700

    Update

--- src/provider.tsx
+++ src/provider.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {StoreContext} from "./context";
 import {Store} from "./store";
 
-export const Provider: React.FC<{value: Store}> = (props) => {
+export const StoreProvider: React.FC<{value: Store}> = (props) => {
   return (
     <StoreContext.Provider value={props.value}>
       {props.children}
